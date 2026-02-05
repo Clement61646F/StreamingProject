6d42acc62f1dd9012fa914827189dfa5452c7f2e
Clement <51334945+Clement61646F@users.noreply.github.com>
2026-02-05 09:28:55 +0100

    Base du projet

---
+++ StreamingProject/StreamingProjectTests/StreamingProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingProjectTests.swift
+//  StreamingProjectTests
+//
+//  Created by Cours on 05/02/2026.
+//
+
+import Testing
+@testable import StreamingProject
+
+struct StreamingProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
